d2dad8d4d63ef873a14be59ef04cd8dcb509f513
Jonas Brand <jonasbrand1712@gmail.com>
2023-12-10 01:05:54 +0100

    Solve puzzle of day 1

--- 01/01.py
+++ 01/01.py
@@ -3,10 +3,41 @@
 import util
 
 
-def main():
+name_to_digit = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+}
+
+
+def digit_from_name(s: str) -> int | None:
+    for name, digit in name_to_digit.items():
+        if s.lower().startswith(name):
+            return digit
+
+
+def main(part: int):
+    calibration_value_sum = 0
     for line in util.iter_input():
-        # continue HERE with your code
+        digits = []
+        for idx, c in enumerate(line):
+            if c.isdigit():
+                digits.append(c)
+            elif part == 2:
+                if digit := digit_from_name(line[idx:]):
+                    digits.append(digit)
+
+        calibration_value_sum += int(f'{digits[0]}{digits[-1]}')
+
+    print(f'{part}) the sum of all calibration values is {calibration_value_sum}')
 
 
 if __name__ == '__main__':
-    main()
+    main(part=1)
+    main(part=2)
