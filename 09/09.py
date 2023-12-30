fee54ba06be62fffb3eddd7d7be56f375625a949
Jonas Brand <jonasbrand1712@gmail.com>
2023-12-30 17:26:47 +0100

    Solve puzzle of day 9

--- 09/09.py
+++ 09/09.py
@@ -3,10 +3,36 @@
 import util
 
 
-def main():
+def numbers_from_str_list(
+    numbers: str,
+) -> list[int]:
+    return [int(number) for number in numbers.split(' ') if number.lstrip('-').isdigit()]
+
+
+def main(part: int):
+    extrapolated_values_sum = 0
+
     for line in util.iter_input():
-        # continue HERE with your code
+        histories = [numbers_from_str_list(numbers=line)]
+
+        while [value for value in histories[len(histories) - 1] if value != 0]:
+            history = histories[len(histories) - 1]
+            next_history = []
+            for idx in range(1, len(history)):
+                next_history.append(history[idx] - history[idx - 1])
+            histories.append(next_history)
+
+        extrapolated_value = 0
+        for history in reversed(histories):
+            if part == 1:
+                extrapolated_value += history[len(history) - 1]
+            elif part == 2:
+                extrapolated_value = history[0] - extrapolated_value
+        extrapolated_values_sum += extrapolated_value
+
+    print(f'{part}) the sum of all extrapolated values is {extrapolated_values_sum}')
 
 
 if __name__ == '__main__':
-    main()
+    main(part=1)
+    main(part=2)
